13e01b585c99dde64b9e974c01c9a0939b54ab76
Roben Kleene <robenk@gmail.com>
2014-02-15 18:07:12 -0500

    Working on getting plugin tests running

--- Contents/Resources/js/wcirb.js
+++
@@ -1,9 +0,0 @@
-WcREPL.oldAddCode = WcREPL.addCode;
-WcREPL.addCode = function(code, source) {
-	var $newcode = this.oldAddCode(code, source);
-	$(document).ready(function() {
-	  $newcode.each(function(i, e) {
-		  hljs.highlightBlock(e);
-	  });
-	});
-};
--- Contents/Resources/wcirb.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require File.join(File.dirname(__FILE__), "lib", "wrapper")
-
-wrapper = WcIRB::Wrapper.new
-
-ARGF.each do |line|
-  wrapper.parse_input(line)
-end
