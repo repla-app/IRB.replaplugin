fd645667217ab7fe97eea21bf5ef172a700d586d
Roben Kleene <contact@robenkleene.com>
2019-02-16 14:02:54 -0500

    Fix some linter errors

--- Contents/Resources/lib/view.rb
+++ Contents/Resources/lib/view.rb
@@ -1,17 +1,22 @@
 require_relative '../bundle/bundler/setup'
 require 'repla/repl'
 
-module Repla::REPL::IRB
-  class View < Repla::REPL::View
-    ROOT_ACCESS_DIRECTORY = File.join(File.dirname(__FILE__), '../html')
-    VIEW_TEMPLATE = File.join(ROOT_ACCESS_DIRECTORY, 'index.html')
+module Repla
+  module REPL
+    module IRB
+      # View
+      class View < Repla::REPL::View
+        ROOT_ACCESS_DIRECTORY = File.join(File.dirname(__FILE__), '../html')
+        VIEW_TEMPLATE = File.join(ROOT_ACCESS_DIRECTORY, 'index.html')
 
-    def initialize
-      super
-      self.root_access_directory_path = File.expand_path(
-        ROOT_ACCESS_DIRECTORY
-      )
-      load_file(VIEW_TEMPLATE)
-    end
+        def initialize
+          super
+          self.root_access_directory_path = File.expand_path(
+            ROOT_ACCESS_DIRECTORY
+          )
+          load_file(VIEW_TEMPLATE)
+        end
+      end
+  end
   end
 end
